c0d2e25ccc89547ce4e2d2bd8d5dd72927c00909
HamzahRushdan <hamzvelli7@gmail.com>
2023-05-06 16:21:44 -0400

    Snake code

--- Snakecode.py
+++ Snakecode.py
@@ -104,3 +104,45 @@ screen.onkeypress(snake_go_right, "Right")
 while True:
     screen.update()
 
+    # snake & fruit collision
+    if snake.distance(fruit) < 20:
+        x = random.randint(-290, 270)
+        y = random.randint(-240, 240)
+        fruit.goto(x, y)
+        scoring.clear()
+        score += 1
+        scoring.write("Score: {}".format(score), align="center", font=("Courier", 24, "bold"))
+        delay -= 0.001
+
+        # Creating new foods
+        new_fruit = turtle.Turtle()
+        new_fruit.speed(0)
+        new_fruit.shape("square")
+        new_fruit.color("red")
+        new_fruit.prenup()
+        old_fruit.append(new_fruit)
+
+            #adding ball to snake
+
+    for index in range(len(old_fruit) -1, 0, -1):
+        a = old_fruit[index -1].xcor()
+        b = old_fruit[index -1].ycor()
+
+        old_fruit[index].goto(a, b)
+
+    if len(old_fruit) > 0:
+        a = snake.xcor()
+        b = snake.ycor()
+        old_fruit[0].goto(a, b)
+    snake_move()
+
+    # snake & border collision
+    if snake.xcor() > 280 or snake.xcor() < -300 or snake.xcor() > 240 or snake.xcor() < -240:
+        time.sleep(1)
+        screen.clear()
+        screen.bgcolor("turquoise")
+        scoring.goto(0,0)
+        scoring.write("             Game Over \n Your score is {}".format(score), align="center", font=("Courier", 30, "bold"))
+
+    # snake collisions
+    for food in
