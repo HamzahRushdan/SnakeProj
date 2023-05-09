0d94d584806e3c81132aa3edf5c667711d859e73
HamzahRushdan <hamzvelli7@gmail.com>
2023-05-09 14:25:02 -0400

    Snake code

--- Snakecode.py
+++ Snakecode.py
@@ -1,22 +1,20 @@
 # My first project in pycharm
-
 #import packages
-#import turtle
-#import random
-#import time
+import turtle
+import random
+import time
 screen = turtle.Screen()
 screen.title("SNAKE GAME")
 screen.setup(width=700, height=700)
 screen.tracer(0)
 screen.bgcolor("#1d1d1d")
 
-
 #creating border
 turtle.speed(10)
 turtle.pensize(4)
-turtle.prenup()
+turtle.penup()
 turtle.goto(-310, 250)
-turtle.pentdown()
+turtle.pendown()
 turtle.color("red")
 turtle.forward(600)
 turtle.right(90)
@@ -27,7 +25,7 @@ turtle.right(90)
 turtle.forward(600)
 turtle.right(90)
 turtle.forward(500)
-turtle.prenup()
+turtle.penup()
 turtle.hideturtle()
 
 # score
@@ -39,7 +37,7 @@ snake = turtle.Turtle()
 snake.speed()
 snake.shape("square")
 snake.color("green")
-snake.prenup()
+snake.penup()
 snake.goto(0, 0)
 snake.direction = 'stop'
 
@@ -48,7 +46,7 @@ fruit = turtle.Turtle()
 fruit.speed(0)
 fruit.shape("square")
 fruit.color("white")
-fruit.prenup()
+fruit.penup()
 fruit.goto(30, 30)
 
 old_fruit = []
@@ -57,7 +55,7 @@ old_fruit = []
 scoring = turtle.Turtle()
 scoring.speed(0)
 scoring.color("white")
-scoring.prenup()
+scoring.penup()
 scoring.hideturtle()
 scoring.goto(0, 300)
 scoring.write("Score: ", align="center", font=("Courier", 24, "bold"))
@@ -67,37 +65,37 @@ def snake_go_up():
     if snake.direction != "down":
         snake.direction = "up"
 
-def snake_go_up():
+def snake_go_down():
     if snake.direction != "up":
         snake.direction = "down"
 
-def snake_go_up():
+def snake_go_left():
     if snake.direction != "right":
         snake.direction = "left"
 
-def snake_go_up():
+def snake_go_right():
     if snake.direction != "left":
         snake.direction = "right"
-def move():
+
+def snake_move():
     if snake.direction == "up":
         y = snake.ycor()
         snake.sety(y + 20)
-
     if snake.direction == "down":
         y = snake.ycor()
-        snake.sety(y + 20)
+        snake.sety(y - 20)
     if snake.direction == "left":
-        y = snake.ycor()
-        snake.sety(y + 20)
+        x = snake.xcor()
+        snake.setx(x + 20)
     if snake.direction == "right":
-        y = snake.ycor()
-        snake.sety(y + 20)
+        x = snake.xcor()
+        snake.setx(x - 20)
 
 # keyboard binding
 screen.listen()
 screen.onkeypress(snake_go_up, "Up")
 screen.onkeypress(snake_go_down, "Down")
-screen.onkeypress(snake_go_left, "left")
+screen.onkeypress(snake_go_left, "Left")
 screen.onkeypress(snake_go_right, "Right")
 
 # main loop
@@ -119,7 +117,7 @@ while True:
         new_fruit.speed(0)
         new_fruit.shape("square")
         new_fruit.color("red")
-        new_fruit.prenup()
+        new_fruit.penup()
         old_fruit.append(new_fruit)
 
             #adding ball to snake
@@ -137,12 +135,21 @@ while True:
     snake_move()
 
     # snake & border collision
-    if snake.xcor() > 280 or snake.xcor() < -300 or snake.xcor() > 240 or snake.xcor() < -240:
+    if snake.xcor() > 280 or snake.xcor() < -300 or snake.ycor() > 240 or snake.ycor() < -240:
         time.sleep(1)
         screen.clear()
         screen.bgcolor("turquoise")
         scoring.goto(0,0)
-        scoring.write("             Game Over \n Your score is {}".format(score), align="center", font=("Courier", 30, "bold"))
+        scoring.write("       Game Over \n Your score is {}".format(score), align="center", font=("Courier", 30, "bold"))
 
     # snake collisions
-    for food in
+    for food in old_fruit:
+        if food.distance(snake) < 20:
+            time.sleep(1)
+            screen.clear()
+            screen.bgcolor("turqoise")
+            scoring.goto(0, 0)
+            scoring.write("     Game Over \n Your score is {}".format(score), align="center", font=("Courier", 30, "bold"))
+
+    time.sleep(delay)
+turtle.Terminator()
